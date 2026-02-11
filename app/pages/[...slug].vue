b571ea4a12096eb0b3f8359397bb8c33661e9e3c
TonyDev92 <tonydeveloper92@gmail.com>
2026-02-11 18:07:00 +0100

    feat(auth):Implement login flow and route protection with middleware

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This page captures all undefined routes and redirects to / (login page)
+definePageMeta({
+    middleware: [
+        function (to) {
+            return navigateTo('/');
+        }
+    ]
+});
+</script>
+
+<template>
+    <div>Redirigiendo...</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'app',
+  css: [
+    '~/assets/scss/_reset.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Esto permite usar $green-rick en cualquier <style> de tus .vue
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;'
+        }
+      }
+    }
+  },
+  modules: [
+    '@nuxt/devtools',
+    '@pinia/nuxt',
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE || 'http://localhost:3000/api'
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
